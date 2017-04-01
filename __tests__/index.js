504d1468cb04e051648f6423acf95a4dce52dd59
Permagate <kend654@gmail.com>
2017-04-01 14:02:56 +0700

    Implement simple client that can send/receive raw message from VNDB API

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,9 +1,61 @@
 /* eslint-env jest */
-const Lib = require('../lib');
+const VNDB = require('../lib');
 
-test('Hello World', async () => {
-  const message = await Lib.hello();
+// Real test against VNDB API
+// So we need to make sue that the timeout interval is big enough
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000; // eslint-disable-line
 
-  expect(message).toBe('Hello World');
+describe('Raw', () => {
+  test('ok', async () => {
+    const vndb = await VNDB.start();
+    const res0 = await vndb.write('login {"protocol":1,"client":"VNDB-Reborn-Tester","clientver":"0.0.1"}');
+    const res1 = await vndb.write('dbstats');
+    const res2 = await vndb.write('get vn basic,anime (id = 17)');
+    const res3 = await vndb.write('invalid command');
+    await vndb.end();
+
+    expect(vndb.socket).toBeTruthy();
+    expect(res0).toBe('ok');
+    expect(res1).toEqual(expect.stringMatching(/^dbstats {.+}/));
+    expect(res2).toEqual(expect.stringMatching(/^results {.+Ever17.+}/));
+    expect(res3).toEqual(expect.stringMatching(/^error {.+parse.+}/));
+    expect(vndb.socket.destroyed).toBe(true);
+  });
+
+  test('ok - multiple messages', async () => {
+    const vndb = await VNDB.start();
+    const res0 = await vndb.write('login {"protocol":1,"client":"VNDB-Reborn-Tester","clientver":"0.0.1"}');
+    const res1 = await Promise.all([
+      vndb.write('dbstats'),
+      vndb.write('get vn basic,anime (id = 17)'),
+      vndb.write('invalid command'),
+    ]);
+    await vndb.end();
+
+    expect(vndb.socket).toBeTruthy();
+    expect(res0).toBe('ok');
+    expect(res1[0]).toEqual(expect.stringMatching(/^dbstats {.+}/));
+    expect(res1[1]).toEqual(expect.stringMatching(/^results {.+Ever17.+}/));
+    expect(res1[2]).toEqual(expect.stringMatching(/^error {.+parse.+}/));
+    expect(vndb.socket.destroyed).toBe(true);
+  });
+
+  test('error - invalid url', async () => {
+    expect.assertions(1);
+    try {
+      await VNDB.start('api-invalid-url.vndb.org');
+    } catch (e) {
+      expect(e).toBeTruthy();
+    }
+  });
+
+  test('error - invalid port', async () => {
+    expect.assertions(1);
+    try {
+      await VNDB.start('api.vndb.org', 19534);
+    } catch (e) {
+      expect(e).toBeTruthy();
+    }
+  });
 });
 
