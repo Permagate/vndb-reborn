c9777c40039780901b70d91e09dfb03d52cb9891
Permagate <kend654@gmail.com>
2017-04-03 20:41:40 +0700

    Add a simple builder

---
+++ __tests__/builder.js
@@ -0,0 +1,172 @@
+/* eslint-env jest */
+const Builder = require('../lib/builder');
+
+describe('Query Builder', () => {
+  describe('login', () => {
+    test('ok', () => {
+      const res0 = Builder.login({
+        client: 'VNDB-Reborn-Tester',
+        clientver: '0.0.1',
+      });
+
+      expect(res0).toEqual(expect.stringContaining('"client":"VNDB-Reborn-Tester"'));
+      expect(res0).toEqual(expect.stringContaining('"clientver":"0.0.1"'));
+
+      const res1 = Builder.login({
+        client: 'VNDB-Reborn-Tester',
+        clientver: '0.0.1',
+        username: 'tester_user',
+        password: 'tester_pass',
+      });
+
+      expect(res1).toEqual(expect.stringContaining('"client":"VNDB-Reborn-Tester"'));
+      expect(res1).toEqual(expect.stringContaining('"clientver":"0.0.1"'));
+      expect(res1).toEqual(expect.stringContaining('"username":"tester_user"'));
+      expect(res1).toEqual(expect.stringContaining('"password":"tester_pass"'));
+    });
+
+    test('error - incomplete argument', () => {
+      try {
+        Builder.login();
+      } catch (e) {
+        expect(e.message).toBe('Login command is missing body argument.');
+      }
+
+      expect.assertions(1);
+    });
+
+    test('error - invalid argument', () => {
+      try {
+        Builder.login('body pls');
+      } catch (e) {
+        expect(e.message).toBe('Login command has non-object body argument.');
+      }
+
+      expect.assertions(1);
+    });
+  });
+
+  describe('dbstats', () => {
+    test('ok', () => {
+      const res0 = Builder.dbstats();
+
+      expect(res0).toBe('dbstats');
+    });
+  });
+
+  describe('get', () => {
+    test('ok', () => {
+      const res0 = Builder.get({
+        type: 'vn',
+        flags: 'basic,anime',
+        filters: '(id = 17)',
+      });
+
+      expect(res0).toBe('get vn basic,anime (id = 17)');
+
+      const res1 = Builder.get({
+        type: 'vn',
+        flags: 'basic, anime',
+        filters: '(id = 17)',
+      });
+
+      expect(res1).toBe('get vn basic,anime (id = 17)');
+
+      const res2 = Builder.get({
+        type: 'vn',
+        flags: ['basic', 'anime'],
+        filters: '(id = 17)',
+      });
+
+      expect(res2).toBe('get vn basic,anime (id = 17)');
+
+      const res3 = Builder.get({
+        type: 'vn',
+        flags: 'basic,anime',
+        filters: 'id = 17',
+      });
+
+      expect(res3).toBe('get vn basic,anime (id = 17)');
+
+      const res4 = Builder.get({
+        type: 'vn',
+        flags: 'basic,anime',
+        filters: '(id = 17)',
+        options: {},
+      });
+
+      expect(res4).toBe('get vn basic,anime (id = 17)');
+
+      const res5 = Builder.get({
+        type: 'vn',
+        flags: 'basic,anime',
+        filters: '(id = 17)',
+        options: {
+          page: 2,
+          results: 10,
+          sort: 'id',
+          reverse: true,
+        },
+      });
+
+      expect(res5).toEqual(expect.stringContaining('get vn basic,anime (id = 17)'));
+      expect(res5).toEqual(expect.stringContaining('"page":2'));
+      expect(res5).toEqual(expect.stringContaining('"results":10'));
+      expect(res5).toEqual(expect.stringContaining('"sort":"id"'));
+      expect(res5).toEqual(expect.stringContaining('"reverse":true'));
+    });
+
+    test('error - incomplete argument', () => {
+      try {
+        Builder.get();
+      } catch (e) {
+        expect(e.message).toBe('Get command is missing type, flags, filters argument(s).');
+      }
+
+      try {
+        Builder.get({
+          type: 'vn',
+          flags: 'basic,anime',
+        });
+      } catch (e) {
+        expect(e.message).toBe('Get command is missing filters argument(s).');
+      }
+
+      try {
+        Builder.get({
+          type: 'vn',
+          filters: '(id = 17)',
+        });
+      } catch (e) {
+        expect(e.message).toBe('Get command is missing flags argument(s).');
+      }
+
+      try {
+        Builder.get({
+          flags: 'basic,anime',
+          filters: '(id = 17)',
+        });
+      } catch (e) {
+        expect(e.message).toBe('Get command is missing type argument(s).');
+      }
+
+      expect.assertions(4);
+    });
+
+    test('error - invalid argument', () => {
+      try {
+        Builder.get({
+          type: 'vn',
+          flags: 'basic,anime',
+          filters: '(id = 17)',
+          options: 'body pls',
+        });
+      } catch (e) {
+        expect(e.message).toBe('Get command has non-object options argument.');
+      }
+
+      expect.assertions(1);
+    });
+  });
+});
+
